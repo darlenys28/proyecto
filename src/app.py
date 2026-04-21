babc6e556d7298f1596a3330a7a925a45f0ae2f1
darlenys <darlenysmesaquintana@gmail.com>
2026-04-21 10:36:11 +0200

    cambios admin

--- src/app.py
+++ src/app.py
@@ -275,7 +275,7 @@ def stripe_webhook():
     payload = request.data
     sig_header = request.headers.get('Stripe-Signature')
 
-    endpoint_secret = 'whsec_fxJbAD4eQFYKJm8g79VtFQ7s3IWuIDUD'
+    endpoint_secret = os.getenv("STRIPE_WEBHOOK_SECRET")
 
     try:
         event = stripe.Webhook.construct_event(
